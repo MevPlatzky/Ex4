4cb4939d6d51a7b48ddffed49c11fd5b29db06dd
mev <ncarmhui@gmail.com>
2025-12-01 11:58:38 +0200

    added Range2D

--- Ex2Sheet.java
+++ Ex2Sheet.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 /**
  * The documentation of this class was removed as of Ex4...
  */
---
+++ Range2D.java
@@ -0,0 +1,4 @@
+package assignments.ex2.ex2_sol;
+
+public class Range2D {
+}
