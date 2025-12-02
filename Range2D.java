7d6b28c09048d9952ab3affc11ad70d2533b72b4
mev <ncarmhui@gmail.com>
2025-12-02 19:00:18 +0200

    added Utils- forms and B_ops added Range2D toString and getCells start.

--- Range2D.java
+++ Range2D.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 
 public class Range2D {
 
-    private Index2D _start;
-    private Index2D _end;
+    private Index2D _first;
+    private Index2D _second;
 
     public Range2D (Index2D a, Index2D b){
+
+        _first = new CellEntry(a.getX(), a.getY());
+        _second = new CellEntry(b.getX(), b.getY());
 
     }
 
@@ -15,14 +18,32 @@ public class Range2D {
         ArrayList<Index2D> Cells = new ArrayList<>();
 
         //pick the right range of cells to add, independently to their insertion order.
-        int minX = Math.min(_end.getX(), _start.getX());
-        int maxX = Math.max(_end.getX(), _start.getX());
-        int minY = Math.min(_end.getY(), _start.getY());
-        int maxY = Math.max(_end.getY(), _start.getY());
+        int minX = Math.min(_second.getX(), _first.getX());
+        int maxX = Math.max(_second.getX(), _first.getX());
+        int minY = Math.min(_second.getY(), _first.getY());
+        int maxY = Math.max(_second.getY(), _first.getY());
 
-        for ()// gets all the relevant cells into the list
 
+
+        // a1:c5 minX = 0 minY = 0, maxX = 2, maxY = 4;
+        //
+
+        for (int i = minX ; i <= maxX; i++) {// gets all the relevant cells into the list
+            for(int j = minY ; j<= maxY ; j++){
+                Index2D tempCell = new CellEntry(i,j);
+                Cells.add(tempCell);
+            }
+        }
         return Cells;
     }
 
+    @Override
+    public String toString() {
+        String ans = "";
+        for (Index2D i: this.getCells()){
+            ans += "("+i.getX()+","+ i.getY()+") | ";
+        }
+        ans = ans.substring(0,ans.length()-3);
+        return ans;
+    }
 }
